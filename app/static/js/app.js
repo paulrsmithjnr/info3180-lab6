e8271eda37647a211c41e9533cf6c80d45ed18e7
Paul Smith <paulrsmithjnr@gmail.com>
2020-04-16 18:13:25 -0500

    Completed lab 6

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -11,10 +11,10 @@ Vue.component('app-header', {
               <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav mr-auto">
                   <li class="nav-item active">
-                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    <router-link to="/" class="nav-link">Home</router-link>
                   </li>
                   <li class="nav-item">
-                    <a class="nav-link" href="#">News</a>
+                    <router-link to="/news" class="nav-link">News</router-link>                  
                   </li>
                 </ul>
               </div>
@@ -41,11 +41,95 @@ Vue.component('app-footer', {
     }
 })
 
+const NewsList = Vue.component('news-list', {
+  template: `
+      <div class="form-inline d-flex justify-content-center">
+        <div class="form-group mx-sm-3 mb-2">
+            <label class="sr-only" for="search">Search</label>
+            <input type="search" name="search" v-model="searchTerm" id="search" class="form-control mb-2 mr-sm-2" placeholder="Enter search term here" />
+            <button class="btn btn-primary mb-2" @click="searchNews">Search</button>
+        </div>
+        <div id="news_articles">
+        </div>
+      </div>
+  `,
+  created: function() {
+    let self = this;
+    fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=<api_key_here>')
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            console.log(data);
+            self.articles = data.articles;
+
+            /*                   My Addition                     */
+            /****************************************************/
+            let html = "<br><div class=\"news text-center\"> <h2>Top Headlines</h2> <ul class=\"news__list\">";
+            self.articles.forEach(article => {
+              html += "<li class=\"news__item\"> <b>" + article.title + "</b> <br> <img class = \"images\" src = \" " + article.urlToImage + "\" alt = \"New image\" /> <br>" + article.description + "<br><br></li>";
+              
+            });
+            html += "</ul> </div>";
+            document.getElementById("news_articles").innerHTML = html;
+            /****************************************************/
+        });
+    }, 
+    data: function() {
+      return {
+        articles: [],
+        searchTerm: '',
+      }
+    },
+    methods: {
+      searchNews: function() {
+      let self = this;
+      fetch('https://newsapi.org/v2/everything?q='+ self.searchTerm + '&language=en&apiKey=<api_key_here>')
+        .then(function(response) {
+          return response.json();
+        })
+        .then(function(data) {
+          console.log(data);
+          self.articles = data.articles;
+
+          /*                   My Addition                     */
+          /****************************************************/
+          let html = "<br><div class=\"news text-center\"> <h2>News articles related to \"" + self.searchTerm + "\"</h2> <ul class=\"news__list\">";
+          self.articles.forEach(article => {
+            html += "<li class=\"news__item\"> <b>" + article.title + "</b> <br> <img class = \"images\" src = \" " + article.urlToImage + "\" alt = \"New image\" /> <br>" + article.description + "<br><br></li>";
+          });
+          html += "</ul> </div>";
+          document.getElementById("news_articles").innerHTML = html;
+          /****************************************************/
+        });
+      }
+    }
+});
+
+const Home = Vue.component('home', {
+  template: `
+      <div class="home text-center">
+          <img src="/static/images/logo.png" alt="VueJS Logo">
+          <h1>{{ welcome }}</h1>
+      </div>
+  `,
+  data: function() {
+    return {
+      welcome: 'Hello World! Welcome to VueJS'
+    }
+  }
+});
+
+const router = new VueRouter({
+  mode: 'history',
+  routes: [
+    { path: '/', component: Home },
+    { path: '/news', component: NewsList }
+  ]
+});
 
 let app = new Vue({
     el: '#app',
-    data: {
-        welcome: 'Hello World! Welcome to VueJS'
-    }
+    router
 });
 
